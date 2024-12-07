6bace30d664e5ca80eff91f9f593ded2902371dd
esik1976 <67402984+esik1976@users.noreply.github.com>
2024-12-07 13:09:20 +0500

    Update customer-register.js

--- customer-register.js
+++ customer-register.js
@@ -1,15 +1,16 @@
-document.getElementById('registrationForm').addEventListener('submit', function (e) {
+document.getElementById('registerForm').addEventListener('submit', (e) => {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Сохраняем данные в localStorage
+    // Сохраняем данные в LocalStorage
     localStorage.setItem('customer_registered', true);
     localStorage.setItem('customer_name', name);
     localStorage.setItem('customer_email', email);
+    localStorage.setItem('customer_password', password);
 
     alert('Регистрация успешна!');
-    window.location.href = './customer-login.html';
+    window.location.href = 'customer.html';
 });
